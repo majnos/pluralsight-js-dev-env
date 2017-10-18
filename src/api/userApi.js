b229a2bd60e98824c4f9275c286b64ad9d81ea42
Jan Tomasek <jan.tomasek@ysoft.com>
2017-10-18 13:36:27 +0200

    revert

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -3,31 +3,31 @@ import getBaseUrl from './baseUrl';
 
 const baseUrl = getBaseUrl();
 
-export function getUsers(){
-    return get('users');
+export function getUsers() {
+  return get('users');
 }
 
 export function deleteUser(id) {
-    return del(`users/${id}`);
+  return del(`users/${id}`);
 }
 
 function get(url) {
-    return fetch(baseUrl + url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
-// Can't call func delete since reserverd word.
+// Can't call func delete since reserved word.
 function del(url) {
-    const request = new Request(baseUrl + url, {
-        method: 'DELETE'
-    });
-    
-    return fetch(request).then(onSuccess, onError);
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
-function onSuccess(response){
-    return response.json();
+function onSuccess(response) {
+  return response.json();
 }
 
-function onError(error){
-    console.log(error); //eslint-disable-line no-console
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
 }
