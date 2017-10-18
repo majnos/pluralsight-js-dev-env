684e07b959e75774e604c86ef41989127b9191a1
Jan Tomasek <jan.tomasek@ysoft.com>
2017-10-18 13:14:27 +0200

    13 nearly finished

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,6 @@
-import  express from 'express';
+import express from 'express';
 import path from 'path';
-import  open from 'open';
+import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
@@ -17,6 +17,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req,res){
+    // Hard coding for simplicity. Pretend this hits a real database
+    res.json([
+            {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+            {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+            {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee@tina@hotmail.com"},
+    ]);
 });
 
 app.listen(port, function(err){
